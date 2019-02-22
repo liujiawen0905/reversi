f7827d28f890df9ec55626eb7772aceab8f5cbca
liujiawen0905 <ljw396439843@gmail.com>
2019-02-21 23:38:55 -0500

    final

--- assets/js/reversi.jsx
+++ assets/js/reversi.jsx
@@ -70,7 +70,7 @@ class Reversi extends React.Component {
       type="spectator";
     }
     this.channel.push("leave", {type: type, user: this.user})
-    window.location.replace("http://127.0.0.1:4000")
+    window.location.replace("http://www.reversi.zy-peng.com")
   }
 
   click(x, y) {
@@ -82,78 +82,108 @@ class Reversi extends React.Component {
        }
     }
   }
-  render() {
-//     console.log(this.state.board)
-     var len=this.state.players.length;
-     var type = ((this.getPlayer(this.user)>0)? "player" : "spectator");
-     return (
-       <div id="main">
-           <RenderBoard id="RenderBoard" board={this.state.board} click={this.click.bind(this)} />
-					 <div>
-					 <h2>Players</h2>
-					 <RenderPlayers players={this.state.players}/>
-					 <h2>Spectators</h2>
-					 <RenderSpectators spectators={this.state.spectators}/>
-					 </div>
-      </div>
-			<div>
-			<button onClick={this.leave.bind(this)}>leave</button>
-			<button onClick={this.reset.bind(this)}>reset</button>
-      </div>
-			</div>
-		 );
-  }
-}
+	leave() {
+	    var type="";
+	    //check if this user is a player
+	    console.log("leave");
+	    if(this.getPlayer(this.user)>=0) {
+	      type="player";
+	    }
+	    else {
+	      type="spectator";
+	    }
+	    this.channel.push("leave", {type: type, user: this.user})
+	    window.location.replace("http://127.0.0.1:4000")
+	  }
 
-function RenderBoard(props) {
-   var result=[];
-   for(var x in props.board) {
-     var row=[];
-     for(var y in props.board[x]) {
-        var target=props.board[x][y];
-        if(target.color=="black") {
-           row.push(<button><img src="/images/black_button.png" alt="black_image"/></button>)
-        }
-        else if(target.color=="white") {
-           row.push(<button><img src="/images/white_button.png" alt="white_image" /></button>)
-        }
-        else {
-           let x_cp=x;
-	   let y_cp=y;
-           row.push(<button onClick={() => props.click(x_cp, y_cp)}> <img src="/images/empty_button.png" alt="empty_image"/></button>)
-        }
-     }
-      result.push(<p>{row}</p>)
-   }
-   return (
-     <div>{result}</div>
-   );
-}
+	  click(x, y) {
+	    let idx = this.getPlayer(this.user);
+	    if(idx>=0) {
+	       if(this.state.players[idx].color==this.state.current_player) {
+	           this.channel.push("click", {x: x, y: y})
+	               .receive("ok", this.init_state.bind(this));
+	       }
+	    }
+	  }
 
-
-function RenderPlayers(props) {
-	var result = [];
-	for(let i in props.players) {
-		let target = props.players[i].name
-
-		result.push(<h1>{target}</h1>)
+	  render() {
+	//     console.log(this.state.board)
+	     var len=this.state.players.length;
+	     var type = ((this.getPlayer(this.user)>0)? "player" : "spectator");
+	     return (
+				 <div>
+				 <RenderWinner players={this.state.players} winner={this.state.winner} />
+	       <div id="main">
+	           <RenderBoard id="RenderBoard" board={this.state.board} click={this.click.bind(this)} />
+						 <div id="list">
+						 <h2>Players</h2>
+						 <RenderPlayers players={this.state.players}/>
+						 <h2>Spectators</h2>
+						 <RenderSpectators spectators={this.state.spectators}/>
+						 </div>
+	      </div>
+				<div>
+				<button id = "btn" onClick={this.leave.bind(this)}>leave</button>
+				<button id = "btn" onClick={this.reset.bind(this)}>reset</button>
+	      </div>
+				</div>
+			 );
+	  }
 	}
-	return (
-		<div>{result}</div>
-	);
-}
 
-function RenderSpectators(props) {
-	var result = [];
-	for(let i in props.spectators) {
-		let target = props.spectators[i].name
-
-		result.push(<h1>{target}</h1>)
+	function RenderBoard(props) {
+	   var result=[];
+	   for(var x in props.board) {
+	     var row=[];
+	     for(var y in props.board[x]) {
+	        var target=props.board[x][y];
+	        if(target.color=="black") {
+	           row.push(<td><button id="game_btn"><img id="btn-image" src="/images/black_button.png" alt="black_image"/>
+						 </button></td>)
+	        }
+	        else if(target.color=="white") {
+	           row.push(<td><button id="game_btn"><img src="/images/white_button.png"
+						 alt="white_image"/></button></td>)
+	        }
+	        else {
+	           let x_cp=x;
+		         let y_cp=y;
+	           row.push(<td><button id="game_btn" onClick={() => props.click(x_cp, y_cp)}>
+						 <img id="empty-img" src="/images/empty_button.png" alt="empty_image"/>
+						 </button></td>)
+	        }
+	     }
+	      result.push(<tr>{row}</tr>)
+	   }
+	   return (
+	     <tb className="board_table">{result}</tb>
+	   );
 	}
-	return (
-		<div>{result}</div>
-	);
-}
+
+
+	function RenderPlayers(props) {
+		var result = [];
+		for(let i in props.players) {
+			let target = props.players[i].name
+
+			result.push(<p>{target}</p>)
+		}
+		return (
+			<div>{result}</div>
+		);
+	}
+
+	function RenderSpectators(props) {
+		var result = [];
+		for(let i in props.spectators) {
+			let target = props.spectators[i].name
+
+			result.push(<p>{target}</p>)
+		}
+		return (
+			<div>{result}</div>
+		);
+	}
 
 function RenderWinner(props) {
   if(props.players.length<2) {
@@ -166,7 +196,3 @@ function RenderWinner(props) {
 	  return (<p id="winner">{props.winner} wins! </p>);
   }
 }
-
-
-
-
